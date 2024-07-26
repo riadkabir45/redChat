9d59a40dac97469812de89c3418e76c8491ed6b1
riadkabir45 <riadkabir45@gmail.com>
2024-07-26 21:15:56 +0600

    fix: long poll and merge targerchat

--- core/views.py
+++ core/views.py
@@ -3,12 +3,71 @@ from django.contrib.auth import login,get_user_model,authenticate
 from .models import CustomUser, ChatTable, ChatLink
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse as JR
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import time
 
-def post(req): 
-    print(req.method)
+
+@receiver(post_save, sender=ChatTable)
+def trg(sender, instance, created, **kwargs):
+    print("New Data")
+
+def index(req):
+    if req.user.id is not None:
+        return redirect(dashboard)
+    return render(req,"index.html")
+
+def logout(req):
+    req.session.flush()
+    return redirect(index)
+
+@login_required(login_url='signin')
+def gchat(req,target=""):
+    oldData = req.session.get("oldData")
+    viewer = req.path.split("/")[-2]
+    if not CustomUser.objects.filter(username=target).exists() and target != "":
+        return redirect(dashboard)
+    while True:
+        if target != "":
+            targetUser = CustomUser.objects.get(username=target)
+            if ChatLink.objects.filter(user1=req.user,user2=targetUser).exists():
+                link = ChatLink.objects.get(user1=req.user,user2=targetUser)
+            elif ChatLink.objects.filter(user2=req.user,user1=targetUser).exists():
+                link = ChatLink.objects.get(user2=req.user,user1=targetUser)
+            else:
+                nlink = ChatLink.objects.create(user2=req.user,user1=targetUser)
+                nlink.save()
+                link = ChatLink.objects.get(user2=req.user,user1=targetUser)
+            ctable = ChatTable.objects.filter(link=link)
+        else:
+            ctable = ChatTable.objects.filter(link__isnull=True)
+        if not ctable.exists():
+            return JR({})
+        ltable = str([elem.id for elem in ctable])
+        if ltable != oldData or viewer != "pchat":
+            break
+        time.sleep(0.5)
+    req.session["oldData"] = ltable
+    res = {}
+    res["user"] = []
+    res["mesg"] = []
+    res["root"] = req.user.username
+    for elem in ctable:
+        res["user"].append(elem.user.username)
+        res["mesg"].append(elem.msg)
+    res["user"].reverse()
+    res["mesg"].reverse()
+    return JR(res)
+
+
+@login_required(login_url='signin')
+def chat(req,target=""):
     if req.method == "POST":
+        print("new respone used")
         msg = req.POST.get("msgtxt")
         target = req.POST.get("target")
+        if msg == None or target == None:
+            return HttpResponse(status=204)
         if target != "":
             targetUser = CustomUser.objects.get(username=target)
             if ChatLink.objects.filter(user1=req.user,user2=targetUser).exists():
@@ -27,100 +86,8 @@ def post(req):
             utxt = msg
             ntext = ChatTable.objects.create(user=req.user,msg=utxt)
             ntext.save()
-    return JR({})
-
-def index(req):
-    req.session['nav'] = {
-                "Home": "dashboard",
-                "Global Chat": "chat",
-                "Local Chat": "target",
-            }
-    req.session['pre_nav'] = {
-            "Home": "index",
-            "Signup": "signup",
-            "Signin": "signin",
-        }
-    if req.user.id is not None:
-        return redirect(dashboard)
-    return render(req,"index.html")
-
-def logout(req):
-    req.session.flush()
-    return redirect(index)
-
-@login_required(login_url='signin')
-def gchatTarget(req,target):
-    if not CustomUser.objects.filter(username=target).exists():
-        return redirect(dashboard)
-    targetUser = CustomUser.objects.get(username=target)
-    if ChatLink.objects.filter(user1=req.user,user2=targetUser).exists():
-        link = ChatLink.objects.get(user1=req.user,user2=targetUser)
-    elif ChatLink.objects.filter(user2=req.user,user1=targetUser).exists():
-        link = ChatLink.objects.get(user2=req.user,user1=targetUser)
-    else:
-        nlink = ChatLink.objects.create(user2=req.user,user1=targetUser)
-        nlink.save()
-        link = ChatLink.objects.get(user2=req.user,user1=targetUser)
-    ctable = ChatTable.objects.filter(link=link)
-    if not ctable.exists():
-        return JR({})
-    res = {}
-    res["user"] = []
-    res["mesg"] = []
-    res["root"] = req.user.username
-    for elem in ctable:
-        res["user"].append(elem.user.username)
-        res["mesg"].append(elem.msg)
-    res["user"].reverse()
-    res["mesg"].reverse()
-    return JR(res)
-
-@login_required(login_url='signin')
-def gchat(req):
-    ctable = ChatTable.objects.filter(link__isnull=True)
-    res = {}
-    res["user"] = []
-    res["mesg"] = []
-    res["root"] = req.user.username
-    for elem in ctable:
-        res["user"].append(elem.user.username)
-        res["mesg"].append(elem.msg)
-    res["user"].reverse()
-    res["mesg"].reverse()
-    return JR(res)
-
-@login_required(login_url='signin')
-def chatTarget(req,target):
-    if not CustomUser.objects.filter(username=target).exists():
-        return redirect(dashboard)
-    targetUser = CustomUser.objects.get(username=target)
-    if ChatLink.objects.filter(user1=req.user,user2=targetUser).exists():
-        link = ChatLink.objects.get(user1=req.user,user2=targetUser)
-    elif ChatLink.objects.filter(user2=req.user,user1=targetUser).exists():
-        link = ChatLink.objects.get(user2=req.user,user1=targetUser)
-    else:
-        nlink = ChatLink.objects.create(user2=req.user,user1=targetUser)
-        nlink.save()
-        link = ChatLink.objects.get(user2=req.user,user1=targetUser)
-
-    print(link)
-    if req.method == "POST":
-        pdata = req.POST
-        utxt = pdata["msgtxt"]
-        ntext = ChatTable.objects.create(user=req.user,msg=utxt,link=link)
-        ntext.save()
-        
+        return HttpResponse(status=204)
     return render(req,"chat.html",{"user":req.user,"target":target})
-
-@login_required(login_url='signin')
-def chat(req):
-    if req.method == "POST":
-        pdata = req.POST
-        utxt = pdata["msgtxt"]
-        ntext = ChatTable.objects.create(user=req.user,msg=utxt)
-        ntext.save()
-        
-    return render(req,"chat.html",{"user":req.user})
 
 def signup(req):
     if req.user.id is not None:
@@ -167,7 +134,7 @@ def target(req):
         uname = pdata["username"]
         redata = {"username": uname}
         if CustomUser.objects.filter(username=uname).exists():
-            return redirect(chatTarget,uname)
+            return redirect(chat,uname)
         redata['msgf'] = "User not found"
         return render(req,"target.html",redata)
     return render(req,"target.html")
